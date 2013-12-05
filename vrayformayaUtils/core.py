d5815a96a00c769dfd7b0d6d1dce83f2b3d3b8dd
Roy Nieterau <roy_nieterau@hotmail.com>
2013-12-05 15:36:44 +0100

    Expanded the addRenderElement's functionality.

--- vrayformayaUtils/core.py
+++ vrayformayaUtils/core.py
@@ -45,7 +45,7 @@ def getRenderElements(renderElements=None, vrayClassType=None):
     return renderElements
 
 
-def addRenderElement(vrayClassType, enabled=True):
+def addRenderElement(vrayClassType, enabled=True, name=None, suffix=None, **kwargs):
     """ Create a V-ray Render Element based on vrayClassType.
 
         Tip: When clicking once on a render element in the "Available Render Elements" list in the render settings it
@@ -56,10 +56,42 @@ def addRenderElement(vrayClassType, enabled=True):
              import maya.cmds as mc
              print mc.vray("getRenderElements")
 
+        :param enabled: Enable/disable the created render element. (default: True)
+        :type  enabled: bool
+
+        :param name: If provided this allows you to set the name of the created render element node.
+        :type  name: None or str
+
+        :param suffix: The filename suffix for the render element. (default: name of the created node)
+        :type  suffix: None or str
+
+        :param kwargs: The attributes that are available on the render element after creation can directly be set
+                       through keyword arguments. Assuming that the attributes start with 'vray'. This is an
+                       experimental implementation but is likely to stay.
+
         :return: The name of the node created
         :rtype: str
     """
     node = mel.eval("vrayAddRenderElement {0}".format(vrayClassType))
+
+    if name is not None:
+        node = mc.rename(node, name)
+
     if not enabled:
         mc.setAttr("{0}.enabled".format(node), enabled)
+
+    if suffix is not None:
+        mc.setAttr("{0}.vray_name_multimatte".format(node), suffix, type="string")
+
+    for kw, value in kwargs:
+        if kw.startswith("vray"):
+            nodeAttr = "{0}.{1}".format(node, kw)
+            if not mc.objExists(nodeAttr):
+                mc.warning("{0} doesn't exist. You have likely entered an invalid attribute keyword.".format(nodeAttr))
+            else:
+                if isinstance(value, basestring):
+                    mc.setAttr(nodeAttr, value, type="string")
+                else:
+                    mc.setAttr(nodeAttr, value)
+
     return node
